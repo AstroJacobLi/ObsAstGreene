3a84efecdacf6f3ab0b3c0edad32854985fbf35a
Jiaxuan Li <jiaxuanl@princeton.edu>
2024-12-10 22:03:24 -0500

    merian project

--- book/docs/utils.py
+++ book/docs/utils.py
@@ -12,7 +12,9 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 IMG_CMAP = copy.copy(matplotlib.cm.get_cmap("viridis"))
 IMG_CMAP.set_bad(color='black')
 
-
+######################################################################
+############## Display related functions ############################
+######################################################################
 def display_single(img,
                    xsize=10,
                    ysize=10,
@@ -129,10 +131,10 @@ def display_single(img,
 
 
 def show_image(image,
-               percl=99, percu=None, vmin=None, vmax=None,
+               percl=99, percu=None, vmin=None, vmax=None, norm=None,
                is_mask=False, figsize=(10, 10),
                cmap='viridis', log=False, clip=True,
-               show_colorbar=True, show_ticks=False,
+               show_colorbar=False, show_ticks=False,
                fig=None, ax=None, input_ratio=None):
     """
     Show an image in matplotlib with some basic astronomically-appropriate stretching.
@@ -211,9 +213,10 @@ def show_image(image,
         interval = aviz.ManualInterval(vmin, vmax)
     else:
         interval = aviz.AsymmetricPercentileInterval(percl, percu)
-    norm = aviz.ImageNormalize(reduced_data,
-                               interval=interval,
-                               stretch=stretch, clip=clip)
+    if norm is None:
+        norm = aviz.ImageNormalize(reduced_data,
+                                interval=interval,
+                                stretch=stretch, clip=clip)
 
     if is_mask:
         # The image is a mask in which pixels should be zero or one.
@@ -245,3 +248,116 @@ def show_image(image,
     if ax is None:
         return fig
     return ax
+
+
+######################################################################
+################ Merian related functions ############################
+######################################################################
+def pad_psf(psf, size=65):
+    """
+    Zero-pad a PSF image such that it has the square shape of `size`.
+    
+    Parameters
+    ----------
+    psf: numpy array
+        The PSF image.
+    size: int
+        The size of the padded PSF.
+    
+    Returns
+    -------
+    padded_psf: numpy array
+        The padded PSF image.
+    """
+    padding = np.array(size)
+    padding -= np.asarray(psf.shape)
+    if not (padding % 2 == 0).all():
+        raise ValueError("Padding requires a non-integer pixel count!")
+    padding = [(pad // 2, pad // 2) for pad in padding]
+    return np.pad(psf, padding)
+
+
+def calc_PSF_FWHM(psfs=None):
+    """
+    Measure the FWHM of the PSF by fitting a 2D Moffat profile to each PSF cutout.
+    
+    Parameters
+    ----------
+    psfs: dict
+        A dictionary containing the PSF images for each band.
+    
+    Returns
+    -------
+    fwhm: dict
+        A dictionary containing the FWHM of the PSF for each band (in pixels).
+    """
+    from astropy.modeling import models, fitting
+    bands = list(psfs.keys())
+
+    fit_p = fitting.LevMarLSQFitter()
+    fwhms = {}
+    for ix, band in enumerate(bands):
+        z = psfs[band]
+        mid = np.array(z.shape) // 2
+        y, x = np.mgrid[: z.shape[0], : z.shape[1]]
+        p_init = models.Moffat2D(x_0=mid[0], y_0=mid[1], amplitude=z[mid[0], mid[1]])
+        p = fit_p(p_init, x, y, z)
+        fwhms[band] = p.fwhm
+
+    return fwhms
+
+def format_object_name(ra=None, dec=None, skycoordobj=None, unit="deg"):
+    """
+    Given a `skycoordobj` object, returns a string representing the object name in the standard form:
+    J{RAhms}{+/-}{DECdms}
+
+    Parameters:
+    -----------
+    ra: float
+        The right ascension of the object in decimal degrees.
+    dec: float
+        The declination of the object in decimal degrees.
+    skycoordobj: `astropy.coordinates.SkyCoord` object
+        The SkyCoord object representing the object. If not provided, it will be created from `ra` and `dec`.
+    unit: str
+        The unit of the input `ra` and `dec`. Default is "deg".
+
+    Returns:
+    --------
+    cname: str
+        A string representing the name of the object in the standard form.
+    """
+    if skycoordobj is None:
+        skycoordobj = SkyCoord(ra, dec, unit=unit)
+    rastring = skycoordobj.ra.to_string(unit=u.hourangle, sep="", precision=2, pad=True)
+    decstring = skycoordobj.dec.to_string(unit=u.deg, sep="", precision=2, pad=True)
+    sign = "+" if skycoordobj.dec > 0 else ""
+    cname = f"J{rastring}{sign}{decstring}"
+    return cname
+
+def get_central_region(cutouts, size):
+    """
+    Extract the central `size x size` region from each cutout in the input dictionary.
+    
+    Parameters:
+        cutouts (dict): A dictionary containing image data arrays.
+        size (int): The size of the central region to extract.
+        
+    Returns:
+        dict: A dictionary with the central region cutouts.
+    """
+    central_cutouts = {}
+    for key, data in cutouts.items():
+        # Calculate the center of the array
+        y_center, x_center = data.shape[0] // 2, data.shape[1] // 2
+        
+        # Calculate the bounds for the central region
+        y_min = max(0, y_center - size // 2)
+        y_max = min(data.shape[0], y_center + size // 2)
+        x_min = max(0, x_center - size // 2)
+        x_max = min(data.shape[1], x_center + size // 2)
+        
+        # Extract the central region
+        central_cutouts[key] = data[y_min:y_max, x_min:x_max]
+    
+    return central_cutouts
