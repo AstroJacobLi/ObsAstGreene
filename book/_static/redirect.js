ab49bdb26d4e3eb048e0788f478d073536afae52
Jiaxuan Li <jiaxuanl@princeton.edu>
2024-12-12 11:04:31 -0500

    password seems to work

--- book/_static/redirect.js
+++ book/_static/redirect.js
@@ -1,2 +1,10 @@
-const basePath = window.location.pathname.split('/').slice(0, -1).join('/');
-window.location.href = `${basePath}/_static/password-protected.html`;
+// const basePath = window.location.pathname.split('/').slice(0, -1).join('/');
+// window.location.href = `${basePath}/../_static/password-protected.html`;
+
+// Check if the user has already authenticated
+if (!localStorage.getItem("authenticated")) {
+    // Redirect to the password-protected page
+    if (window.location.pathname === "/" || window.location.pathname.endsWith("index.html")) {
+        window.location.href = "../_static/password-protected.html";
+    }
+}
